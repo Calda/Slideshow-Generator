558489e1030e4532502c14a262ae3da0ac806ada
Calda <calstephens98@gmail.com>
2015-03-05 12:44:52 -0500

    Initial Commit

---
+++ Slideshow Generator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slideshow Generator
+//
+//  Created by DFA Film 9: K-9 on 3/5/15.
+//  Copyright (c) 2015 Cal Stephens. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
